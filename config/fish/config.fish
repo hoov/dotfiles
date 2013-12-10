ed8de4e63883df0ff627dfc09f52897ad8eaa138
Tom Hoover <thoover@alum.mit.edu>
2013-12-10 11:39:05 -0500

    A few minor tweaks for tmux and fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,7 @@
 set fish_color_user 'AF5FFF'
 set fish_color_host 'FF6600'
 set fish_color_cwd '00FF00'
+set fish_color_status 'FF0000'
 
 set __fish_git_prompt_show_informative_status 'yes'
 
@@ -13,6 +14,9 @@ set __fish_git_prompt_color_branch '5FD7FF'
 # We'll take care of it ourselves...
 set VIRTUAL_ENV_DISABLE_PROMPT 'yes'
 
+# Prefer /usr/local/bin
+set PATH /usr/local/bin $PATH
+
 set -x JAVA_HOME (/usr/libexec/java_home)
 
 if test -e $HOME/.awscreds
@@ -23,3 +27,8 @@ if test -e /usr/local/Library/LinkedKegs/aws-iam-tools/jars
     set -x AWS_IAM_HOME /usr/local/Library/LinkedKegs/aws-iam-tools/jars
 end
 
+# rbenv setup
+set PATH ~/.rbenv/bin $PATH
+. (rbenv init -|psub)
+
+set -x P4CONFIG '.p4rc'
