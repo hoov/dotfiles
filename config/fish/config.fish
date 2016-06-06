38b61975b5e8384d657af2bd36622b39808e7558
Tom Hoover <thoover@insightsquared.com>
2016-06-06 15:49:23 -0400

    Fix stupid typo

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -40,4 +40,4 @@ if test -f $HOME/.gpg-agent-info
     eval (sed -e 's/^\(.*\)=\(.*\)$/set \1 \2/' ~/.gpg-agent-info | tr '\n' '; ')
 end
 
-eval sh $HOME/.config/base16-shell/base16-default.solarized.sh
+eval sh $HOME/.config/base16-shell/base16-solarized.dark.sh
